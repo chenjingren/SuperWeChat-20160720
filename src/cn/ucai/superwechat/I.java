4767829fd7ed0dd30f27de0bd739c64644a0e9c0
chenjingren <1182508339@qq.com>
2016-07-22 00:20:01 +0800

    修改SERVER_ROOT和applicationId

--- src/cn/ucai/superwechat/I.java
+++ src/cn/ucai/superwechat/I.java
@@ -2,7 +2,7 @@ package cn.ucai.superwechat;
 
 public interface I {
 
-	String SERVER_ROOT = "http:10.0.2.2:8080/SuperWeChatServer/Server";
+	String SERVER_ROOT = "http://10.0.2.2:8080/SuperWeChatServer/Server";
 	String QUESTION = "?";
 	String EQU = "=";
 	String MSG_PREFIX_MSG = "msg_";
