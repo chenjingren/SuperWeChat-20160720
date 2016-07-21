5452ae15abb7b0f81cb4a498531a0c047fa42a86
chenjingren <1182508339@qq.com>
2016-07-21 20:07:57 +0800

    在I接口中添加消息码前缀的字段,在Utils工具类中有用到

--- src/cn/ucai/superwechat/I.java
+++ src/cn/ucai/superwechat/I.java
@@ -5,6 +5,7 @@ public interface I {
 	String SERVER_ROOT = "http:10.0.2.2:8080/SuperWeChatServer/Server";
 	String QUESTION = "?";
 	String EQU = "=";
+	String MSG_PREFIX_MSG = "msg_";
 
 	interface User {
 		String TABLE_NAME							=		"t_superwechat_user";
