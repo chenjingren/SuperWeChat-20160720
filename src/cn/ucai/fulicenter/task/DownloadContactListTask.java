141d2e53605a6e90102b45a8dc0c4fd0615f86ce
chenjingren <1182508339@qq.com>
2016-08-02 13:09:20 +0800

    将包名改为cn.ucai.superwechat

---
+++ src/cn/ucai/fulicenter/task/DownloadContactListTask.java
@@ -0,0 +1,61 @@
+package cn.ucai.fulicenter.task;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.SuperWeChatApplication;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.bean.UserAvatar;
+import cn.ucai.fulicenter.utils.OkHttpUtils2;
+import cn.ucai.fulicenter.utils.Utils;
+
+/**
+ * Created by Administrator on 2016/7/22 0022.
+ */
+public class DownloadContactListTask {
+    public static final String TAG = DownloadContactListTask.class.getName();
+
+    private String username;
+    Context mContext;
+
+    public DownloadContactListTask(Context mContext, String username) {
+        this.mContext = mContext;
+        this.username = username;
+    }
+
+    public void execute(){
+        OkHttpUtils2<String> utils2 = new OkHttpUtils2<String>();
+        utils2.setRequestUrl(I.REQUEST_DOWNLOAD_CONTACT_ALL_LIST)
+                .addParam(I.Contact.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(new OkHttpUtils2.OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        Log.e(TAG,"s===="+s);
+                        Result result = Utils.getListResultFromJson(s, UserAvatar.class);
+                        Log.e(TAG,"result===="+result);
+                        ArrayList<UserAvatar> userList = (ArrayList<UserAvatar>) result.getRetData();
+                        if (userList!=null&&userList.size()>0){
+                            Log.e(TAG,"userList.size()==="+userList.size());
+                            SuperWeChatApplication.getInstance().setUserList(userList);
+                            mContext.sendStickyBroadcast(new Intent("update_contact_list"));
+
+                            Map<String,UserAvatar> userAvatar = SuperWeChatApplication.getInstance().getContactMap();
+                            for (UserAvatar user:userList){
+                                userAvatar.put(user.getMUserName(),user);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e(TAG,"error==="+error);
+                    }
+                });
+    }
+}
