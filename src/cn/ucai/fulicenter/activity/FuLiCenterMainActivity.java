97974151a006faa10b33c982b3311f3163c040f3
chenjingren <1182508339@qq.com>
2016-08-03 16:19:38 +0800

    新建FuLiCenterMainActivity,布局，在清单文件中注册

---
+++ src/cn/ucai/fulicenter/activity/FuLiCenterMainActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.fulicenter.activity;
+
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+
+public class FuLiCenterMainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fu_li_center_main);
+
+    }
+}
