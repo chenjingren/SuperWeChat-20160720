821d6522882d619a4f11ae9a39b56918b902adbd
chenjingren <1182508339@qq.com>
2016-08-06 01:49:48 +0800

    ‘初始化商品详情布局的控件，并在清单文件中注册GoodDetailsA

--- src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailsActivity.java
@@ -2,14 +2,55 @@ package cn.ucai.fulicenter.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.view.FlowIndicator;
+import cn.ucai.fulicenter.view.SlideAutoLoopView;
 
 public class GoodDetailsActivity extends Activity {
+
+    public static final String TAG = GoodDetailsActivity.class.getName();
+
+    ImageView ivShare,ivCollect,ivCart;
+    TextView tvCartCount;
+
+    TextView tvGoodEnglishName,tvGoodName,tvCurrentPrice,tvShopPrice;
+
+    SlideAutoLoopView salv;
+
+    FlowIndicator indicator;
+
+    WebView wvGoodBrief;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good_details);
+        initView();
+    }
+
+    private void initView() {
+        ivShare = (ImageView) findViewById(R.id.iv_share);
+        ivCollect = (ImageView) findViewById(R.id.iv_collect);
+        ivCart = (ImageView) findViewById(R.id.iv_cart);
+
+        tvCartCount = (TextView) findViewById(R.id.tvCartCount);
+
+        tvGoodEnglishName = (TextView) findViewById(R.id.tv_goods_english_name);
+        tvGoodName = (TextView) findViewById(R.id.tv_goods_name);
+        tvCurrentPrice = (TextView) findViewById(R.id.tv_goods_price_current);
+        tvShopPrice = (TextView) findViewById(R.id.tv_goods_price_shop);
+
+        salv = (SlideAutoLoopView) findViewById(R.id.salv);
+        indicator = (FlowIndicator) findViewById(R.id.indicator);
+        wvGoodBrief = (WebView) findViewById(R.id.wv_good_brief);
+        WebSettings settings =
+                wvGoodBrief.getSettings();
+        settings.setBuiltInZoomControls(true);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
     }
 }
